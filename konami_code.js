28b1ead55f2fdab9bf2a5c5637d91c8c83f8cbea
Jerry P <jpull@jpull.com>
2018-02-16 04:08:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,10 +16,10 @@ document.addEventListener('keydown', function(e) {
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
-    if (key === alphabet[index]) {
+    if (key === code[index]) {
       index++;
 
-      if (index === alphabet.length) {
+      if (index === code.length) {
         alert("Hurray!");
 
         index = 0;
