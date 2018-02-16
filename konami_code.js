bc9bb778d264c45418f8098fbde986dd470e4f1c
Jerry P <jpull@jpull.com>
2018-02-16 04:22:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,16 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
 
   document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.which || e.detail);
 
-    if (key === code[index]) {
+    if (code[index] === key) {
       index++;
 
       if (index === code.length) {
-        alert("Hurray!");
+        alert('Hurray!');
 
         index = 0;
       }
@@ -18,4 +18,4 @@ function init() {
       index = 0;
     }
   });
-  }
+}
