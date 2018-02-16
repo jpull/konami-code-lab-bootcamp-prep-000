d4fb19a9f7bc960a767310e85968c389e4987633
Jerry P <jpull@jpull.com>
2018-02-16 04:01:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,3 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 }
-
-  const input = document.querySelector('input')
-
-  input.addEventListener('keydown', function(e) {
-    console.log(e.which)
-  })
