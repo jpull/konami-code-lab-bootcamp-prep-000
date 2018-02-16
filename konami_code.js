0320da5a42fb24212dc6837a39ebb77fad541726
Jerry P <jpull@jpull.com>
2018-02-16 04:24:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,29 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+}
+document.addEventListener('keydown', function(e) {
+    onKeyDownHandler(e)
+  })
+  // Key codes for A, B, and C keys.
+  const alphabet = [65, 66, 67];
+
+  // Keep track of index outside of the event handler.
   let index = 0;
 
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail);
+  // This is the function that would be invoked by the event listener.
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
 
-    if (code[index] === key) {
+    if (key === code[index]) {
       index++;
 
       if (index === code.length) {
-        alert('Hurray!');
+        alert("Hurray!");
 
         index = 0;
       }
     } else {
       index = 0;
     }
-  });
-}
+  }
